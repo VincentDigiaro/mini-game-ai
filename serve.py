a1795e3f925797445a284fed0d6005a570402072
cid <vincent.digiaro@gmail.com>
2024-05-07 10:43:39 +0200

    server part

---
+++ serve.py
@@ -0,0 +1,88 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+from urllib.parse import parse_qs
+import subprocess
+import re
+import requests
+
+
+jsonHistory = []
+
+class CORSHTTPRequestHandler(BaseHTTPRequestHandler):
+    def _set_headers(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        # Autoriser toutes les origines
+        self.send_header('Access-Control-Allow-Origin', '*')
+        # Gérer les requêtes pré-envoi (Preflight)
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type')
+        self.end_headers()
+
+    def do_GET(self):
+        self._set_headers()
+        response_message = "Hello! It's nice to meet you. Is there something I can help you with, or would you like to chat?"
+        self.wfile.write(response_message.encode('utf-8'))
+
+    def do_POST(self):
+        # Récupère la longueur des données
+        
+        self._set_headers()       
+
+        
+        content_length = int(self.headers['Content-Length'])
+        
+        # Lit les données postées et les décode en JSON
+        post_data = self.rfile.read(content_length).decode('utf-8')
+        post_vars = json.loads(post_data)
+        
+        # Extrait la valeur associée à 'key'
+        question = post_vars.get('question')
+        
+        print(question)
+        url = 'http://localhost:11434/api/chat'
+        headers = {'Content-Type': 'application/json'}
+        
+        userQuestion = {"role": "user", "content": question}
+        jsonHistory.append(userQuestion)
+        
+        print(str(jsonHistory))
+        data = json.dumps({
+            "model": "wizard",
+            "stream": False,
+            "messages": jsonHistory
+        })
+
+        response = requests.post(url, headers=headers, data=data)
+        
+        message = json.loads(response.text)    
+       # result = result.strip()    
+        result =  message.get('message').get('content')
+        
+        botResponse = {"role": "assistant", "content": result}
+   
+        jsonHistory.append(botResponse)
+        
+            
+        # Affiche et répond à la requête
+
+        self.send_response(200)  # 200 OK
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        response_data = json.dumps({"message": f"{result}"})
+        self.wfile.write(response_data.encode('utf-8'))
+
+    def do_OPTIONS(self):
+        self._set_headers()
+        # Pas besoin d'ajouter plus de contenu ici, les en-têtes sont suffisants
+
+def run(server_class=HTTPServer, handler_class=CORSHTTPRequestHandler):
+    server_address = ('', 8080)
+    httpd = server_class(server_address, handler_class)
+    print('Serveur démarré, utilisez <Ctrl-C> pour arrêter')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
+
+
